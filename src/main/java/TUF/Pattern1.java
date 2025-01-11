068e9b6d9ae7c22e5b7113eca5f1e8291269946d
Pradhumna Gaudel <pradhumnagaudel0@gmail.com>
2025-01-11 10:24:24 +0530

    pattern1.java

---
+++ src/main/java/TUF/Pattern1.java
@@ -0,0 +1,12 @@
+package TUF;
+//https://www.naukri.com/code360/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=SUBMISSION
+public class Pattern1 { public static void nForest(int n) {
+    for (int i = 0;i<n;i++){
+        for (int j = 0;j<n;j++){
+            System.out.print("* ");
+        }
+        System.out.println(" ");
+    }
+    // Write your code here
+}
+}
