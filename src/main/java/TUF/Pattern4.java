7d5c73cb6245dfe7c2d7969bb55e6c28998589ee
Pradhumna Gaudel <pradhumnagaudel0@gmail.com>
2025-01-11 10:55:49 +0530

    pattern4.java

---
+++ src/main/java/TUF/Pattern4.java
@@ -0,0 +1,15 @@
+package TUF;
+//www.naukri.com/code360/problems/triangle_6573690?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=SUBMISSION
+public class Pattern4 {
+    public static void nTriangle(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(+i+" ");
+            }
+            System.out.println("");
+        }
+
+        // Write your code here
+    }
+}
+
