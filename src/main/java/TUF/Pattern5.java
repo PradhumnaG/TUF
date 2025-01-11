af0be511c6a04219045a42e941035a188349cbb1
Pradhumna Gaudel <pradhumnagaudel0@gmail.com>
2025-01-11 11:07:40 +0530

    pattern5.java

---
+++ src/main/java/TUF/Pattern5.java
@@ -0,0 +1,14 @@
+package TUF;
+//https://www.naukri.com/code360/problems/seeding_6581892?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTabValue=SUBMISSION
+public class Pattern5 {
+    public static void seeding(int n) {
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+
+            }
+            System.out.println("");
+        }
+        // Write your code here
+    }
+}
