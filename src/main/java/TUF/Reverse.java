36df66c02b7965f69e21cabdcc19a15413c5a51c
Pradhumna Gaudel <pradhumnagaudel0@gmail.com>
2025-01-17 22:31:16 +0530

    Reverse.java

---
+++ src/main/java/TUF/Reverse.java
@@ -0,0 +1,21 @@
+package TUF;
+
+
+    public class chalo(int x) {
+        int a = x;
+        int b = 0;
+        int arr [] = new int[x];
+        while(a!=0){
+            arr[b] = a %10;
+            b++;
+
+        }
+        a/=10;
+
+    }
+
+    return arr[];
+}
+
+
+}
