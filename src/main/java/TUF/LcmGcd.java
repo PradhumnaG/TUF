df6aa47d54c5cb761f6b3ba651893a261a15c2de
Pradhumna Gaudel <pradhumnagaudel0@gmail.com>
2025-01-19 18:14:36 +0530

    LcmGcd.java

---
+++ src/main/java/TUF/LcmGcd.java
@@ -0,0 +1,45 @@
+package TUF;
+
+Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class  LcmGcd{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int a;
+            a = Integer.parseInt(br.readLine());
+
+            int b;
+            b = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            int[] ans = obj.lcmAndGcd(a, b);
+            System.out.println(ans[0] + " " + ans[1]);
+            System.out.println("~");
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+
+    public static int[] lcmAndGcd(int a, int b) {
+        int m=a,n=b;
+        while(b!=0)
+        {
+            int r=a%b;
+            a=b;
+            b=r;
+        }
+        int gcd=a;
+        int lcm=(m*n)/gcd;
+        return new int[]{lcm,gcd};
+    }
