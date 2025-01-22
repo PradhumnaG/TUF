15a7203d1aa34b771c318f768ad061dcc283a82f
Pradhumna Gaudel <pradhumnagaudel0@gmail.com>
2025-01-22 10:10:36 +0530

    IsPrime.java

---
+++ src/main/java/TUF/IsPrime.java
@@ -0,0 +1,27 @@
+package TUF;
+
+public class IsPrime {
+    public static String isPrime(int num) {
+
+        //Your code goes here
+
+        if(num<=1){
+
+            return "NO";
+
+        }
+
+        for(int i=2;i<=Math.sqrt(num);i++){
+
+            if(num%i == 0){
+
+                return "NO";
+
+            }
+
+        }
+
+        return "YES";
+
+    }
+}
